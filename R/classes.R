61a57b28216360394dcdee9f961176b5646df297
Duncan Temple Lang <duncan@r-project.org>
2019-08-06 16:01:51 -0700

    adding methods that are implementation-independent.  Exploring the S3/S4 interface limitations

--- R/classes.R
+++ R/classes.R
@@ -1,7 +1,13 @@
 
+if(FALSE) {
 setClass("Document", contains = "VIRTUAL")
 setClass("DocumentPage", contains = "VIRTUAL")
-
+} else {
+ setOldClass("Document")
+ setOldClass("DocumentPage")
+}
+    
+setClass("TextBoundingBox", contains = "data.frame")
 
 
 setGeneric("getTextBBox",
@@ -18,6 +24,122 @@ setGeneric("getPages",
 
 
 # Default method, but has to inherit from the Document class so doesn't apply to everything.
+# Will work for any specific Document class so does not need to be implemented, but
+# can be overridden for efficiency, e.g. to use XPath to get the number without creating the
+# page objects.
 setMethod("getNumPages", "Document",
           function(doc, ...)
-            length(doc))
+            length(getPages(doc)))
+
+
+
+
+if(TRUE) {
+setGeneric("margins",
+           function(obj, asDataFrame = TRUE, ...) {  # get signature correct
+              standardGeneric("margins")
+          })
+
+
+setMethod("margins", "Document",
+function(obj, asDataFrame = TRUE, ...)        
+{
+    ans = lapply(getPages(obj), margins)
+    if(asDataFrame)
+        as.data.frame(do.call(rbind, ans))
+    else
+        ans
+})
+
+setMethod("margins", "DocumentPage",
+margins.DocumentPage <- function(obj, asDataFrame = TRUE, ...)        {
+       margins(as(obj, "TextBoundingBox"), asDataFrame, ...)
+   })
+
+
+
+
+#setMethod("margins", "TextBoundingBox",
+#function(obj, asDataFrame = TRUE, ...) {
+#         c(left = min(bbox$x), right = max(bbox$x + bbox$width))
+#     })
+
+setMethod("margins", "TextBoundingBox",
+function(obj, asDataFrame = TRUE, ...) {
+         c(left = min(obj$left), right = max(obj$left + obj$width))
+     })
+} else {
+
+    # S3 generic and methods.
+margins =    
+function(obj, asDataFrame = TRUE)
+    UseMethod("margins")
+
+
+margins.Document =
+function(obj, asDataFrame = TRUE, ...)        
+{
+    ans = lapply(getPages(obj), margins)
+    if(asDataFrame)
+        as.data.frame(do.call(rbind, ans))
+    else
+        ans
+}
+
+margins.DocumentPage =
+function(obj, asDataFrame = TRUE, ...)        {
+       margins(as(obj, "TextBoundingBox"), asDataFrame, ...)
+}
+
+
+
+#setMethod("margins", "TextBoundingBox",
+#function(obj, asDataFrame = TRUE, ...) {
+#         c(left = min(bbox$x), right = max(bbox$x + bbox$width))
+#     })
+
+margins.TextBoundingBox =
+    function(obj, asDataFrame = TRUE, ...) {
+        #XXXX
+         c(left = left(bbox), right = right(bbox))
+     }
+}
+
+
+
+
+setGeneric("left", function(x, ...) standardGeneric("left"))
+setGeneric("right", function(x, ...) standardGeneric("left"))
+setMethod("left", "TextBoundingBox", function(x, ...) x$x)
+
+
+if(FALSE) {
+###############
+# Package specific code.
+# In ReadPDF,
+setMethod("right", "PDFTextBoundingBox", function(x, ...) x$y1)
+# In Rtesseract
+setMethod("right", "OCRTextBoundingBox", function(x, ...) x$x + x$width)
+
+
+setAs("OCRPage", "TextBoundingBox",
+         function(from) {
+              GetBoxes(from)
+          })
+
+
+
+setAs("PDFToXMLPage", "TextBoundingBox",
+         function(from) {
+             getTextBBox(from, asDataFrame = TRUE)
+             # getBBox2(from, asDataFrame = TRUE)
+          })
+}
+
+
+dim.Document =
+function(x)
+  t(sapply(getPages(x), dim))
+
+# Validity
+# 
---
+++ tests/testPDF.R
@@ -0,0 +1,21 @@
+
+if(require(ReadPDF)) {
+
+d = readPDFXML("SamplePDFs/Amada-2013.xml")
+
+class(d)
+
+length(d)
+getNumPages(d)
+dm = dim(d)
+stopifnot(is.matrix(dm) && ncol(dm) == 2 && all(colnames(dm) == c("width", "height")))
+
+pgs = getPages(d)
+x = d[[1]]
+class(x)
+stopifnot(identical(pgs[[1]], x))
+
+dim(x)
+
+margins(x)
+}
