0e8b59b430d7baac164f63f5be05628a8d3256c6
Duncan Temple Lang <duncan@r-project.org>
2019-08-08 13:21:15 -0700

    plot methods for drawing a page

---
+++ R/plot.R
@@ -0,0 +1,71 @@
+
+
+setMethod("plot", "Document",
+function(x, y, mar = c(1, 1, 2, 1), ...)
+{
+    np = getNumPages(x)
+    r = ceiling(sqrt(np))
+    c = ceiling(np/r)
+    opar = par(no.readonly = TRUE)
+    on.exit(par(opar))
+    par(mfrow = c(r, c), mar = mar)
+    invisible(mapply(function(p, i, ...) plot(p, main = paste0("Page ", i), ...), getPages(x), seq(1, np), MoreArgs = list(...)))
+})
+
+#XXX Compute colors
+# pageTitle() function and default value.
+setMethod("plot", "DocumentPage",
+function(x, y, colors = getTextColors(x), ...)
+{
+    #?? Should this be
+    #   getTextBBox(x, color = TRUE)
+    # or
+    #  as(x, "TextBoundingBox")
+    # Or should getTextBBox() have color, rotation, pages, etc. all be TRUE.
+    bb = as(x, "TextBoundingBox") # getTextBBox(x, color = TRUE)
+    plot(bb, pageHeight =  getPageHeight(x), colors = colors, ...)
+})
+
+setMethod("plot", "TextBoundingBox",
+function(x, y, pageHeight = max(bottom(x)), colors = getTextColors(x), ...)
+{    
+    plot(1, xlim = range(c(left(x), right(x))), ylim = range(0, pageHeight), ..., xlab = "", ylab = "", axes = FALSE)
+    box()
+    text(left(x), pageHeight - bottom(x), x$text, adj = 0, cex = .5, col = colors)
+})
+
+
+if(FALSE) {
+library(Dociface); library(ReadPDF)
+doc = readPDFXML(list.files("SamplePDFs", pattern = "Amada-2013.xml", full = TRUE))
+plot(doc[[1]])
+plot(doc[[1]], colors = "red")
+plot(doc[[1]], colors = c("red", "green"))
+}
+
+setGeneric("getTextColors",
+             function(obj, ...) 
+               standardGeneric("getTextColors"))
+
+
+setMethod("getTextColors", "ANY",
+          function(obj, ...)
+               "black")
+
+
+
+getPageHeight =
+function(obj, ...)
+    UseMethod("getPageHeight")
+
+getPageHeight.DocumentPage =
+function(obj, ...)
+    dim(obj)[1]
+
+getPageWidth =
+function(obj, ...)
+    UseMethod("getPageWidth")
+
+getPageWidth.DocumentPage =
+function(obj, ...)
+   dim(obj)[2]
